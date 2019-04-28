c364cdfdd09956a91c365db501b9739782c792fe
suxiang2 <lunamystic>
2019-04-27 22:22:20 -0500

    init

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+const admin = require('firebase-admin');
+const acc = require('./api/ServiceAccountKey.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(acc),
+  databaseURL: "https://pet-adoption-498rk.firebaseio.com"
+});
+
+const db = admin.firestore();
+
 const allRoutes = './api/routes';
 const productRoutes = require(allRoutes + '/products')
 
